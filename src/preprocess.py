bdc3dff586883669790a199fe0f3fe41a54799c7
Pavan Kumar <rdpkumar2308@gmail.com>
2026-02-09 16:43:44 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,12 @@
+import nltk
+import string
+from nltk.corpus import stopwords
 
+nltk.download('stopwords')
+
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    tokens = text.split()
+    tokens = [t for t in tokens if t not in stopwords.words('english')]
+    return " ".join(tokens)
